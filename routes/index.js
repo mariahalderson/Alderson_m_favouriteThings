ce05a933ae147c74622b8ea69191ba6bffe27992
Mariah Alderson <mariah@Mariahs-MacBook-Pro.local>
2019-03-24 16:51:12 -0400

    dynamimic data, db calls, routing set

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var connect = require('../utils/sqlConnect');
+var mysql      = require('mysql');
+var async      = require('async');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,16 +17,47 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/:hero', function(req, res, next) {
+router.get('/:thing', function(req, res, next) {
   //call db for individual data
-  connect.query(`SELECT * FROM hero WHERE name = "${req.params.hero}"`, (err, result) => {
-    if(err){
-      throw err;
-      console.log(err);
-    }else{
-      res.render('bio', { biodata: result[0] });
-    }
-  });
+  // ;
+  // connect.query(`SELECT * FROM tbl_favourites f, tbl_images i WHERE  f.id = "${req.params.thing}" AND i.thing_id="${req.params.thing}"`, (err, result) => {
+  // // connect.query(`SELECT * FROM tbl_favourites WHERE id = "${req.params.thing}"`, (err, result) => {
+  //   if(err){
+  //     throw err;
+  //     console.log(err);
+  //   }else{
+  //     console.log(result);
+  //     res.render('info', { thing: result[0] });
+  //   }
+  // });
+  // var pool = mysql.createPool(credentials);
+   var query1 = `SELECT * FROM tbl_favourites WHERE id = '${req.params.thing}'`;
+   var query2 = `SELECT * FROM tbl_images WHERE thing_id = '${req.params.thing}'`;
+
+   var return_data = {};
+
+   async.parallel([
+      function(parallel_done) {
+          connect.query(query1, {}, function(err, results) {
+              if (err) return parallel_done(err);
+              return_data.thing = results;
+              parallel_done();
+          });
+      },
+      function(parallel_done) {
+          connect.query(query2, {}, function(err, results) {
+              if (err) return parallel_done(err);
+              return_data.images = results;
+              parallel_done();
+          });
+      }
+   ], function(err) {
+        if (err) console.log(err);
+        // pool.end();
+        console.log(return_data);
+        // res.send(return_data);
+        res.render('info', { thing: return_data.thing[0], images: return_data.images });
+   });
 });
 
 module.exports = router;
